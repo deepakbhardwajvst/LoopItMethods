d5eda4cf6850af78bd8237ae1685ccf3f02a751f
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-03-21 20:02:40 +0530

    Update Login component - Added login UI with Google sign-in option and input fields for email and password. Implemented remember me switch.

---
+++ app/component/input/InputText.js
@@ -0,0 +1,21 @@
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
+const InputText = ({ typetext, placeholdertext }) => {
+  return (
+    <>
+      <input
+        className="w-[402px] my-3 py-4 px-4 block  rounded-[10px] shadow text-lg font-semibold placeholder:text-[14px] placeholder:text-[#5A5A5A] placeholder:font-normal outline-none"
+        type={typetext}
+        name=""
+        id=""
+        placeholder={placeholdertext}
+      />
+      {typetext === "password" && (
+        <div className="absolute top-[29px] right-6">
+          <VisibilityOffOutlinedIcon className="text-[#B8B8B8]" />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default InputText;
