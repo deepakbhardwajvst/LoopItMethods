045bd7efbc4d489ba353a86fd1d89bd69df057d4
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-03-26 21:16:56 +0530

    Add conditional rendering for editing, delete, edit, save, and create child note functionalities.

--- app/component/category/Category.js
+++ app/component/category/Category.js
@@ -19,7 +19,63 @@ const Category = ({ note, onDelete, onEdit, onSave, onCreateChild }) => {
   };
   return (
     <div className="bg-[#A5B74A] p-4  shadow-md ">
-      
+      {editing ? (
+        <textarea
+          className="w-full h-24 p-2 rounded-md"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+      ) : (
+        <p>{note.content}</p>
+      )}
+      <div className="mt-2 flex justify-between">
+        <div>
+          <button
+            className="bg-[#093e10] text-white p-1 mr-1  ml-4"
+            onClick={() => onDelete(note.id)}
+          >
+            Delete
+          </button>
+          <button
+            className="bg-[#093e10] text-white p-1 mr-1"
+            onClick={handleEdit}
+          >
+            Edit
+          </button>
+        </div>
+        <div>
+          {editing ? (
+            <button
+              className="bg-[#133d14] text-white px-2 py-1 rounded-md"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          ) : (
+            <button
+              className="bg-[#093e10] text-white p-1 mr-1"
+              onClick={handleCreateChild}
+            >
+              New Notes
+            </button>
+          )}
+        </div>
+      </div>
+      {note.children && note.children.length > 0 && (
+        <div className="mt-4">
+          {note.children.map((childNote) => (
+            <div key={childNote.id} className="ml-4">
+              <Category
+                note={childNote}
+                onDelete={onDelete}
+                onEdit={onEdit}
+                onSave={onSave}
+                onCreateChild={onCreateChild}
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
